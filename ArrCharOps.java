948569c112ae11878b4fe0685109a08f74744b4b
MohamadAbedAlkader <mam10102004@gmail.com>
2024-11-29 18:03:43 +0200

    Beta-7

--- ArrCharOps.java
+++ ArrCharOps.java
@@ -92,25 +92,29 @@ public class ArrCharOps {
     }
 
     public static int compareTo(String str1, String str2) {
+        // Handle invalid input
         if (str1 == null || str2 == null) {
-            return -2;
+            return -2; // Error code for null input
         }
 
+        // Compare characters one by one
         int minLength = Math.min(str1.length(), str2.length());
-
         for (int i = 0; i < minLength; i++) {
-            if (str1.charAt(i) != str2.charAt(i)) {
-                return str1.charAt(i) > str2.charAt(i) ? 1 : -1;
+            char char1 = str1.charAt(i);
+            char char2 = str2.charAt(i);
+            if (char1 != char2) {
+                return char1 < char2 ? -1 : 1;
             }
         }
 
-        if (str1.length() > str2.length()) {
-            return 1;
-        } else if (str1.length() < str2.length()) {
-            return -1;
+        // If all characters match, compare lengths
+        if (str1.length() < str2.length()) {
+            return -1; // Shorter string is lexicographically smaller
+        } else if (str1.length() > str2.length()) {
+            return 1; // Longer string is lexicographically greater
         }
 
-        return 0;
+        return 0; // Strings are equal
     }
 }
 
