5efdc03c70756499d7921c31ecb1779aaaf00c05
MohamadAbedAlkader <mam10102004@gmail.com>
2024-11-29 18:18:41 +0200

    Beta-7

--- Primes.java
+++ Primes.java
@@ -1,45 +1,26 @@
-import java.util.ArrayList;
-
 public class Primes {
     public static void main(String[] args) {
-        int n = Integer.parseInt(args[0]);
-        boolean[] isPrime = new boolean[n + 1];
-        ArrayList<Integer> primes = new ArrayList<>();
+        System.out.println( "Prime numbers up to " + (args [0] + ":"));
+        int bound = Integer.parseInt(args [0]);
         int count = 0;
-
-        // Initialize isPrime array
-        for (int i = 2; i <= n; i++) {
-            isPrime[i] = true;
-        }
-
-        // Sieve of Eratosthenes to mark non-prime numbers
-        for (int i = 2; i * i <= n; i++) {
-            if (isPrime[i]) {
-                for (int j = i * i; j <= n; j += i) {
-                    isPrime[j] = false;
+        int i = 2;
+        while (i <= bound) {
+            if (i == 2) {
+                System.out.println(i);
+                count ++;
+            } else {
+                int div = i -1;
+                while (i % div != 0) {
+                    div --;
+                    if (div == 1) {
+                        count ++;
+                        System.out.println(i);
+                        break;
+                    }
                 }
-            }
+            } i++;
         }
+        System.out.println("There are " + count + " primes between 2 and " + bound + " (" + ((count * 100) / bound) + "% are primes)");
 
-        // Collect primes and count them
-        for (int i = 2; i <= n; i++) {
-            if (isPrime[i]) {
-                primes.add(i);
-                count++;
-            }
-        }
-
-        // Print prime numbers
-        System.out.println("Prime numbers up to " + n + ":");
-        for (int prime : primes) {
-            System.out.println(prime);
-        }
-
-        // Correctly calculate total numbers in range [2, n]
-        int totalNumbers = n - 1; // Total numbers between 2 and n (inclusive)
-        double percentage = (count / (double) totalNumbers) * 100;
-
-        // Print count and percentage
-        System.out.printf("There are %d primes between 2 and %d (%.0f%% are primes)%n", count, n, percentage);
     }
 }
