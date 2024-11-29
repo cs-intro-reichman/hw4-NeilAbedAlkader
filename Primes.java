3abc529251c0cdba33225406010f081187592185
MohamadAbedAlkader <mam10102004@gmail.com>
2024-11-29 18:00:43 +0200

    Beta-7

--- Primes.java
+++ Primes.java
@@ -7,10 +7,12 @@ public class Primes {
         ArrayList<Integer> primes = new ArrayList<>();
         int count = 0;
 
+        // Initialize isPrime array
         for (int i = 2; i <= n; i++) {
             isPrime[i] = true;
         }
 
+        // Sieve of Eratosthenes to mark non-prime numbers
         for (int i = 2; i * i <= n; i++) {
             if (isPrime[i]) {
                 for (int j = i * i; j <= n; j += i) {
@@ -19,6 +21,7 @@ public class Primes {
             }
         }
 
+        // Collect primes and count them
         for (int i = 2; i <= n; i++) {
             if (isPrime[i]) {
                 primes.add(i);
@@ -26,11 +29,17 @@ public class Primes {
             }
         }
 
+        // Print prime numbers
         System.out.println("Prime numbers up to " + n + ":");
         for (int prime : primes) {
             System.out.println(prime);
         }
 
-        System.out.println("There are " + count + " primes between 2 and " + n + ":");
+        // Calculate percentage of primes
+        int totalNumbers = n - 1; // Total numbers in range [2, n]
+        double percentage = (count / (double) totalNumbers) * 100;
+
+        // Print count and percentage
+        System.out.printf("There are %d primes between 2 and %d (%.0f%% are primes)%n", count, n, percentage);
     }
 }
