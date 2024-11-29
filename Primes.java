bb4b0121a5cc67492275c1c8a3a1855f667bd0c8
MohamadAbedAlkader <mam10102004@gmail.com>
2024-11-29 17:46:21 +0200

    Beta-7

--- Primes.java
+++ Primes.java
@@ -7,12 +7,10 @@ public class Primes {
         ArrayList<Integer> primes = new ArrayList<>();
         int count = 0;
 
-        // Initialize isPrime array
         for (int i = 2; i <= n; i++) {
             isPrime[i] = true;
         }
 
-        // Sieve of Eratosthenes to mark non-prime numbers
         for (int i = 2; i * i <= n; i++) {
             if (isPrime[i]) {
                 for (int j = i * i; j <= n; j += i) {
@@ -21,7 +19,6 @@ public class Primes {
             }
         }
 
-        // Collect primes and count them
         for (int i = 2; i <= n; i++) {
             if (isPrime[i]) {
                 primes.add(i);
@@ -29,13 +26,11 @@ public class Primes {
             }
         }
 
-        // Print prime numbers
-        System.out.println("Prime numbers up to " + n );
+        System.out.println("Prime numbers up to " + n + ":");
         for (int prime : primes) {
             System.out.println(prime);
         }
 
-        // Print count of primes
-        System.out.println("There are " + count + " primes between 2 and " + n );
+        System.out.println("There are " + count + " primes between 2 and " + n + ":");
     }
 }
