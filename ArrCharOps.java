fdcfdda0e721eadff50eac27e38ce0121418c5a2
MohamadAbedAlkader <mam10102004@gmail.com>
2024-11-29 16:20:00 +0200

    Beta-2

--- ArrCharOps.java
+++ ArrCharOps.java
@@ -92,7 +92,11 @@ public class ArrCharOps {
     }
 
     public static int compareTo(String str1, String str2) {
+        if (str1 == null || str2 == null) {
+            return -2;
+        }
         int minLength = Math.min(str1.length(), str2.length());
+
         for (int i = 0; i < minLength; i++) {
             if (str1.charAt(i) != str2.charAt(i)) {
                 return str1.charAt(i) - str2.charAt(i);
