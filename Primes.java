127ca6ce2e68de986308346acb0652ef6a8a5c06
MohamadAbedAlkader <mam10102004@gmail.com>
2024-11-29 16:48:44 +0200

    Beta-3

--- Primes.java
+++ Primes.java
@@ -16,7 +16,7 @@ public class Primes {
                 }
             }
         }
-
+        System.out.println("Prime numbers up to " + n + ":");
         for (int i = 2; i <= n; i++) {
             if (isPrime[i]) {
                 System.out.println(i);
