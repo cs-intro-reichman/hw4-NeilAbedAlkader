80ece84f5e4510add9bc09048319097e24fcd16b
NeilAbedAlkader <mam10102004@gmail.com>
2024-12-05 20:17:10 +0200

    Update Primes.java

--- Primes.java
+++ Primes.java
@@ -3,22 +3,22 @@ public class Primes {
         System.out.println( "Prime numbers up to " + (args [0] + ":"));
         int bound = Integer.parseInt(args [0]);
         int count = 0;
-        int i = 2;
-        while (i <= bound) {
-            if (i == 2) {
-                System.out.println(i);
+        int num1 = 2;
+        while (num1 <= bound) {
+            if (num1 == 2) {
+                System.out.println(num1);
                 count ++;
             } else {
-                int num = i -1;
-                while (i % num != 0) {
+                int num = num1 -1;
+                while (num1 % num != 0) {
                     num --;
                     if (num == 1) {
                         count ++;
-                        System.out.println(i);
+                        System.out.println(num1);
                         break;
                     }
                 }
-            } i++;
+            } num1++;
         }
         System.out.println("There are " + count + " primes between 2 and " + bound + " (" + ((count * 100) / bound) + "% are primes)");
 
